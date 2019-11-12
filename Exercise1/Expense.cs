fd8d5bc2ef9ba4365fe0d4861c816877ba130551
Paraic Mc Donagh <S00188844@mail.itsligo.ie>
2019-11-12 15:40:12 +0000

    Add project files.

---
+++ Exercise1/Expense.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    public class Expense
+    {
+        //Properties
+        public decimal Amount  { get; set; }
+        public DateTime ExpenseData { get; set; }
+        public string category { get; set; }
+
+
+        //Constructors - will return later
+
+        //Methods
+
+
+    }
+}
